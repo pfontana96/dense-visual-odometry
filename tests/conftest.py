ed29f925c9c87fe7cb215d64474cbf28a8a28710
Pedro Fontana <pedro.fontana.1996@gmail.com>
2022-08-28 22:59:51 -0300

    Adds ground truth for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import json
 
+import numpy as np
 import cv2
 
 import pytest
@@ -9,18 +11,34 @@ _TEST_DATADIR = Path(__file__).resolve().parent / "test_data"
 
 
 @pytest.fixture
-def load_rgb_image(image_name: str):
-    filename = _TEST_DATADIR / "rgb" / image_name
-    return cv2.imread(str(filename), cv2.IMREAD_GRAYSCALE)
+def load_rgb_image(filepath: str):
+    return cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
 
 
 @pytest.fixture
-def load_depth_image(image_name: str):
-    filename = _TEST_DATADIR / "depth" / image_name
-    return cv2.imread(str(filename), cv2.IMREAD_ANYDEPTH)
+def load_depth_image(filepath: str):
+    return cv2.imread(filepath, cv2.IMREAD_ANYDEPTH)
 
 
 @pytest.fixture
 def load_camera_intrinsics_file():
     filepath = _TEST_DATADIR / "test_camera_intrinsics.yaml"
     return filepath
+
+
+@pytest.fixture
+def load_test_benchmark():
+
+    # Load ground truth
+    with (_TEST_DATADIR / "ground_truth.json").open("r") as fp:
+        ground_truth_data = json.load(fp)
+
+    rgb_images = []
+    depth_images = []
+    ground_truth_transformations = []
+    for value in ground_truth_data.values():
+        rgb_images.append(load_rgb_image(str(_TEST_DATADIR / value["rgb"])))
+        depth_images.append(load_rgb_image(str(_TEST_DATADIR / value["depth"])))
+        ground_truth_transformations.append(np.array(value["transformation"]))
+
+    return rgb_images, depth_images, ground_truth_transformations
