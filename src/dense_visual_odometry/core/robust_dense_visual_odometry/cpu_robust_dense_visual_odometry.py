Fix when approximating jacobian on image2

src/dense_visual_odometry/core/robust_dense_visual_odometry/cpu_robust_dense_visual_odometry.py
@@ -3,6 +3,7 @@ import logging
 
 import numpy as np
 import numpy.typing as npt
+import numba as nb
 from scipy.interpolate import RegularGridInterpolator
 
 from dense_visual_odometry.core.robust_dense_visual_odometry.base_robust_dvo import BaseRobustDVO
@@ -87,12 +88,33 @@ class RobustDVOCPU(BaseRobustDVO):
 
             gradx, grady = compute_gradients(image=self._prev_gray_image_pyr.at(level), kernel_size=3)
 
+            # gradx_values = gradx[mask].reshape(-1, 1)
+            # grady_values = grady[mask].reshape(-1, 1)
+
+            # self._jacobian = np.empty((gradx_values.size, 6), dtype=np.float32)
+            # for i, gradients in enumerate(np.hstack((gradx_values, grady_values))):
+            #     self._jacobian[i] = np.dot(gradients.reshape(1, 2), J_w[i])
             gradx_values = gradx[mask].reshape(-1, 1)
             grady_values = grady[mask].reshape(-1, 1)
 
-            J = np.empty((gradx_values.size, 6), dtype=np.float32)
-            for i, gradients in enumerate(np.hstack((gradx_values, grady_values))):
-                J[i] = np.dot(gradients.reshape(1, 2), J_w[i])
+            gradients = np.ascontiguousarray(np.hstack((gradx_values, grady_values)))
+
+            self._jacobian = self._compute_jacobian_approximate_image2_gradient(
+                np.ascontiguousarray(J_w), gradients
+            )
+
+    @staticmethod
+    @nb.njit("float32[:,:](float32[:,:,:], float32[:,:])", parallel=True, fastmath=True)
+    def _compute_jacobian_approximate_image2_gradient(
+        Jw: npt.NDArray[np.float32], gradients: npt.NDArray[np.uint8]
+    ) -> npt.NDArray[np.float32]:
+
+        N = gradients.shape[0]
+        jacobian = np.empty((N, 6), dtype=np.float32)
+        for i in nb.prange(N):
+            jacobian[i] = gradients[i] @ Jw[i]
+
+        return jacobian
 
     def _cleanup(self):
         self._gray_image_interpolator = None

src/dense_visual_odometry/utils/jacobian.py
@@ -1,14 +1,13 @@
 import numpy as np
-# import cupy as cp
+import numpy.typing as npt
+import numba as nb
 import cv2
 
-from dense_visual_odometry.utils.numpy_cache import np_cache
 
-
-@np_cache
+@nb.njit("float32[:,:,:](float32[:,:], float32[:,:])", parallel=True, fastmath=True)
 def compute_jacobian_of_warp_function(
-    pointcloud: np.ndarray, calibration_matrix: np.ndarray, use_gpu: bool = False
-) -> np.ndarray:
+    pointcloud: npt.NDArray[np.float32], calibration_matrix: npt.NDArray[np.float32]
+) -> npt.NDArray[np.float32]:
     """
         Computes the Jacobian of a warp function
 
@@ -28,19 +27,19 @@ def compute_jacobian_of_warp_function(
     fx = calibration_matrix[0, 0]
     fy = calibration_matrix[1, 1]
 
-    x = pointcloud[0, :]
-    y = pointcloud[1, :]
-    z = pointcloud[2, :]
+    N = pointcloud.shape[1]
 
-    zeros = np.zeros_like(x)
+    J_w = np.empty((N, 2, 6), dtype=np.float32)
 
-    J_w = np.array([
-        [fx / z, zeros, -fx * x / z ** 2, -fx * (x * y) / z ** 2, fx * (1 + (x ** 2 / z ** 2)), -fx * y / z],
-        [zeros, fy / z, -fy * y / z ** 2, -fy * (1 + (y ** 2 / z ** 2)), fy * (x * y) / z ** 2, fy * x / z]
-    ], dtype=np.float32)
+    for i in nb.prange(N):
+        x = pointcloud[0, i]
+        y = pointcloud[1, i]
+        z = pointcloud[2, i]
 
-    # Transpose array to be of shape Nx2x6
-    J_w = np.transpose(J_w, [2, 0, 1])
+        J_w[i] = np.array([
+            [fx / z, 0.0, -fx * x / z ** 2, -fx * (x * y) / z ** 2, fx * (1 + (x ** 2 / z ** 2)), -fx * y / z],
+            [0.0, fy / z, -fy * y / z ** 2, -fy * (1 + (y ** 2 / z ** 2)), fy * (x * y) / z ** 2, fy * x / z]
+        ], dtype=np.float32)
 
     return J_w
 